9aae7fa1f32e29939b1bd43b0b1f02519d18d473
YOUR_NAME <YOUR_EMAIL>
2024-10-24 02:23:40 +0300

    -

--- 10.3.py
+++ 10.3.py
@@ -1,3 +1,21 @@
+class Talo:
+    def __init__(self, alin_kerros, ylin_kerros, hissien_lukumäärä):
+        self.alin_kerros = alin_kerros
+        self.ylin_kerros = ylin_kerros
+        self.hissit = [Hissi(alin_kerros, ylin_kerros) for _ in range(hissien_lukumäärä)]
+
+    def aja_hissiä(self, hissin_numero, kohde_kerros):
+        if 0 <= hissin_numero < len(self.hissit):
+            self.hissit[hissin_numero].siirry_kerrokseen(kohde_kerros)
+        else:
+            print("Virheellinen hissin numero!")
+
+    def palohälytys(self):
+        print("Palohälytys! Kaikki hissit menevät pohjakerrokseen.")
+        for hissi in self.hissit:
+            hissi.siirry_kerrokseen(self.alin_kerros)
+
+
 class Hissi:
     def __init__(self, alin_kerros, ylin_kerros):
         self.alin_kerros = alin_kerros
@@ -30,21 +48,9 @@ class Hissi:
             self.kerros_alas()
 
 
-class Talo:
-    def __init__(self, alin_kerros, ylin_kerros, hissien_lukumäärä):
-        self.alin_kerros = alin_kerros
-        self.ylin_kerros = ylin_kerros
-        self.hissit = [Hissi(alin_kerros, ylin_kerros) for _ in range(hissien_lukumäärä)]
-
-    def aja_hissiä(self, hissin_numero, kohde_kerros):
-        if 0 <= hissin_numero < len(self.hissit):
-            self.hissit[hissin_numero].siirry_kerrokseen(kohde_kerros)
-        else:
-            print("Virheellinen hissin numero!")
-
-
 talo = Talo(1, 10, 3)
 
 talo.aja_hissiä(0, 5)
 talo.aja_hissiä(1, 8)
 talo.aja_hissiä(2, 1)
+talo.palohälytys()

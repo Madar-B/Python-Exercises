4aa56b8b9789ad2488ed9567cd302508d09ee214
YOUR_NAME <YOUR_EMAIL>
2024-10-24 02:09:09 +0300

    -

---
+++ 10.2.py
@@ -0,0 +1,44 @@
+class Talo:
+    def __init__(self, alin_kerros, ylin_kerros, hissien_lukumäärä):
+        self.alin_kerros = alin_kerros
+        self.ylin_kerros = ylin_kerros
+        self.hissit = [Hissi(alin_kerros, ylin_kerros) for _ in range(hissien_lukumäärä)]
+
+    def aja_hissiä(self, hissin_numero, kohde_kerros):
+        if 0 <= hissin_numero < len(self.hissit):
+            self.hissit[hissin_numero].siirry_kerrokseen(kohde_kerros)
+        else:
+            print("Virheellinen hissin numero!")
+
+
+class Hissi:
+    def __init__(self, alin_kerros, ylin_kerros):
+        self.alin_kerros = alin_kerros
+        self.ylin_kerros = ylin_kerros
+        self.nykyinen_kerros = alin_kerros
+        print(f"Hissi on alimmassa kerroksessa: {self.nykyinen_kerros}")
+
+    def kerros_ylös(self):
+        if self.nykyinen_kerros < self.ylin_kerros:
+            self.nykyinen_kerros += 1
+            print(f"Hissi siirtyi kerrokseen {self.nykyinen_kerros}")
+        else:
+            print("Hissi on jo ylimmässä kerroksessa!")
+
+    def kerros_alas(self):
+        if self.nykyinen_kerros > self.alin_kerros:
+            self.nykyinen_kerros -= 1
+            print(f"Hissi siirtyi kerrokseen {self.nykyinen_kerros}")
+        else:
+            print("Hissi on jo alimmassa kerroksessa!")
+
+    def siirry_kerrokseen(self, kohde_kerros):
+        if kohde_kerros > self.ylin_kerros or kohde_kerros < self.alin_kerros:
+            print("Virheellinen kerros!")
+            return
+
+        while self.nykyinen_kerros < kohde_kerros:
+            self.kerros_ylös()
+        while self.nykyinen_kerros > kohde_kerros:
+            self.kerros_alas()
+

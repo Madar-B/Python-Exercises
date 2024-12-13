0e967f45e56a0b9467afee65a403abf3d3ecf40d
YOUR_NAME <YOUR_EMAIL>
2024-12-13 11:04:27 +0200

    Initial commit

---
+++ 3.js
@@ -0,0 +1,10 @@
+'use strict';
+const names = ['John', 'Paul', 'Jones'];
+
+
+const target = document.getElementById('target')
+
+for (let i = 0; i < names.length; i++) {
+
+ target.innerHTML += `<li>${names[i]}</li>`;
+}
